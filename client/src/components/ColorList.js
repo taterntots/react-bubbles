54ab7591acd19c769cbac01d0c361aa7a2dbc98a
taterntots <matthew.a.weidner@gmail.com>
2019-12-20 12:10:03 -0500

    put request saves edited data, but only on refreshing the page. want to make it instantaneous

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const initialColor = {
   color: '',
@@ -7,9 +7,11 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
+  // console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+
+  console.log(colorToEdit);
 
   const editColor = color => {
     setEditing(true);
@@ -18,9 +20,11 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
-    // Make a put request to save your updated color
-    // think about where will you get the id from...
-    // where is is saved right now?
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(res => {
+        console.log(res.data);
+      })
   };
 
   const deleteColor = color => {
