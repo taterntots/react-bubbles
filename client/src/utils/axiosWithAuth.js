a9398c7c473b74b06f010e9259b46b5b8d9d409f
taterntots <matthew.a.weidner@gmail.com>
2019-12-20 12:02:36 -0500

    successfully created a get request that displays data from the server

--- client/src/utils/axiosWithAuth.js
+++ client/src/utils/axiosWithAuth.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-    const token = localStorage.getItem('toke');
+    const token = localStorage.getItem('token');
     return axios.create({
         baseURL: 'http://localhost:5000/api',
-        header: {
+        headers: {
             Authorization: token
         }
     })
